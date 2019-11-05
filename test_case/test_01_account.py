a8f4e518679b4ede618469320f9e2b11f287acd3
unknown <huanghongliang@codemao.cn>
2019-11-05 15:51:28 +0800

    清空

--- test_case/test_01_account.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from page.page_first import FirstPage
-
-
-def test_login_by_ac_with_right_author(init_test):
-    p_first = FirstPage()
-    p_login = p_first.tap_login_by_ac_bt()
-    p_login.login('18339956220', 'qwe123')
